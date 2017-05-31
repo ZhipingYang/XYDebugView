5caa8ba60f8982fb8680ec191049de83728475e9
Daniel <xcodeyang@gmail.com>
2017-06-01 01:50:54 +0800

    optimize gesture track

---
+++ Demo/XYDebugView/NSObject+Runtime.m
@@ -0,0 +1,73 @@
+//
+//  NSObject+Runtime.m
+//  ProtectCrash
+//
+//  Created by XcodeYang on 03/05/2017.
+//  Copyright © 2017 XcodeYang. All rights reserved.
+//
+
+#import "NSObject+Runtime.h"
+#import <objc/runtime.h>
+
+char * const kProtectCrashProtectorName = "kProtectCrashProtector";
+
+void ProtectCrashProtected(id self, SEL sel) {
+}
+
+@implementation NSObject (Runtime)
+
+// MARK: Util
++ (void)swizzleClassMethodWithOriginSel:(SEL)oriSel swizzledSel:(SEL)swiSel {
+    Class cls = object_getClass(self);
+    
+    Method originAddObserverMethod = class_getClassMethod(cls, oriSel);
+    Method swizzledAddObserverMethod = class_getClassMethod(cls, swiSel);
+    
+    [self swizzleMethodWithOriginSel:oriSel oriMethod:originAddObserverMethod swizzledSel:swiSel swizzledMethod:swizzledAddObserverMethod class:cls];
+}
+
++ (void)swizzleInstanceMethodWithOriginSel:(SEL)oriSel swizzledSel:(SEL)swiSel {
+    Method originAddObserverMethod = class_getInstanceMethod(self, oriSel);
+    Method swizzledAddObserverMethod = class_getInstanceMethod(self, swiSel);
+    
+    [self swizzleMethodWithOriginSel:oriSel oriMethod:originAddObserverMethod swizzledSel:swiSel swizzledMethod:swizzledAddObserverMethod class:self];
+}
+
++ (void)swizzleMethodWithOriginSel:(SEL)oriSel
+                         oriMethod:(Method)oriMethod
+                       swizzledSel:(SEL)swizzledSel
+                    swizzledMethod:(Method)swizzledMethod
+                             class:(Class)cls {
+    BOOL didAddMethod = class_addMethod(cls, oriSel, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+    
+    if (didAddMethod) {
+        class_replaceMethod(cls, swizzledSel, method_getImplementation(oriMethod), method_getTypeEncoding(oriMethod));
+    } else {
+        method_exchangeImplementations(oriMethod, swizzledMethod);
+    }
+}
+
++ (Class)addMethodToStubClass:(SEL)aSelector {
+    Class ProtectCrashProtector = objc_getClass(kProtectCrashProtectorName);
+    
+    if (!ProtectCrashProtector) {
+        ProtectCrashProtector = objc_allocateClassPair([NSObject class], kProtectCrashProtectorName, sizeof([NSObject class]));
+        objc_registerClassPair(ProtectCrashProtector);
+    }
+    
+    class_addMethod(ProtectCrashProtector, aSelector, (IMP)ProtectCrashProtected, "v@:");
+    return ProtectCrashProtector;
+}
+
+- (BOOL)isMethodOverride:(Class)cls selector:(SEL)sel {
+    IMP clsIMP = class_getMethodImplementation(cls, sel);
+    IMP superClsIMP = class_getMethodImplementation([cls superclass], sel);
+    
+    return clsIMP != superClsIMP;
+}
+
++ (BOOL)isMainBundleClass:(Class)cls {
+    return cls && [[NSBundle bundleForClass:cls] isEqual:[NSBundle mainBundle]];
+}
+
+@end
--- Demo/XYDebugView/UIWindow+Runtime.m
+++ Demo/XYDebugView/UIWindow+Runtime.m
@@ -9,43 +9,39 @@
 #import "UIWindow+Runtime.h"
 #import "FORGestureTrack.h"
 #import <objc/runtime.h>
+#import "NSObject+Runtime.h"
 
 @implementation UIWindow (Runtime)
 
 + (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        Class class = [self class];
-        // When swizzling a class method, use the following:
-        // Class class = object_getClass((id)self);
-        
-        SEL originalSelector = @selector(makeKeyAndVisible);
-        SEL swizzledSelector = @selector(xy_makeKeyAndVisible);
-        
-        Method originalMethod = class_getInstanceMethod(class, originalSelector);
-        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
-        
-        BOOL didAddMethod =
-        class_addMethod(class,
-                        originalSelector,
-                        method_getImplementation(swizzledMethod),
-                        method_getTypeEncoding(swizzledMethod));
-        
-        if (didAddMethod) {
-            class_replaceMethod(class,
-                                swizzledSelector,
-                                method_getImplementation(originalMethod),
-                                method_getTypeEncoding(originalMethod));
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
+        [self swizzleInstanceMethodWithOriginSel:@selector(makeKeyAndVisible) swizzledSel:@selector(xy_makeKeyAndVisible)];
+        [self swizzleInstanceMethodWithOriginSel:@selector(becomeKeyWindow) swizzledSel:@selector(xy_becomeKeyWindow)];
+        [self swizzleInstanceMethodWithOriginSel:@selector(makeKeyWindow) swizzledSel:@selector(xy_makeKeyWindow)];
     });
 }
 
 - (void)xy_makeKeyAndVisible
 {
     [self xy_makeKeyAndVisible];
-    
+    [self startTrack];
+}
+
+- (void)xy_becomeKeyWindow
+{
+    [self xy_becomeKeyWindow];
+    [self startTrack];
+}
+
+- (void)xy_makeKeyWindow
+{
+    [self xy_makeKeyWindow];
+    [self startTrack];
+}
+
+- (void)startTrack
+{
     FORGestureTrack* track = [[FORGestureTrack alloc] initWithFrame:self.window.bounds];
     track.dotWidth = 40;
     track.layer.zPosition = CGFLOAT_MAX;
